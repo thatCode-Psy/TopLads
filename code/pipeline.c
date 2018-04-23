6b27b852b6caf4636d06dbcd1943c090a5c83ef8
David May <38572912+Maydit@users.noreply.github.com>
2018-04-23 12:39:09 -0400

    pipeline_cycles

--- code/pipeline.c
+++ code/pipeline.c
@@ -67,7 +67,7 @@ void iplc_sim_push_pipeline_stage()
     }
     
     /* 5. Increment pipe_cycles 1 cycle for normal processing */
-    
+    pipeline_cycles++;
     /* 6. push stages thru MEM->WB, ALU->MEM, DECODE->ALU, FETCH->ALU */
     pipeline[WRITEBACK] = pipeline[MEM];
     pipeline[MEM] = pipeline[ALU];
